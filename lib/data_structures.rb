428d556fb392aa9bfd3d9bca56c125d6c0cf4f5c
Rahoul Baruah <rahoulb@echodek.co>
2024-10-31 16:32:28 +0000

    Added type_for accessor
    For ease of access

--- lib/data_structures.rb
+++ lib/data_structures.rb
@@ -3,25 +3,17 @@ require "data_structures/version"
 require "data_structures/engine"
 
 module DataStructures
-  def self.[] name
-    await { @configuration[name].value }
-  end
+  def self.[](name) = await { @configuration[name].value }
 
-  def self.register name, class_or_name
-    @configuration.register name, class_or_name
-  end
+  def self.register(name, class_or_name) = @configuration.register name, class_or_name
 
-  def self.class_for name
-    await { @configuration.class_for name }
-  end
+  def self.class_for(name) = await { @configuration.class_for name }
 
-  def self.load config
-    @item_loader.call config
-  end
+  def self.load(config) = @item_loader.call config
 
-  def self.reset
-    @configuration.reset
-  end
+  def self.type_for(klass) = await { @configuration.type_for klass }
+
+  def self.reset = @configuration.reset
 
   require_relative "data_structures/configuration"
   require_relative "data_structures/template_collection"
