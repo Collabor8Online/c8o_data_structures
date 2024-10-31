50b9170b6a729c33d046436b2ca73d2ffe7d4a28
Rahoul Baruah <rahoulb@echodek.co>
2024-10-31 13:59:59 +0000

    Only allow legal default values for dates
    today
    Or +/-n d/w/m/y

--- lib/data_structures/definitions/date_field.rb
+++ lib/data_structures/definitions/date_field.rb
@@ -2,6 +2,9 @@ module DataStructures
   module Definitions
     class DateField < Field
       attribute :default, :string, default: ""
+      validate :default do |date_field|
+        date_field.errors.add :default, :is_not_legal_date_default unless date_field.default == "today" || date_field.default.match?(/\A-?\d+[dwmy]\z/)
+      end
     end
   end
 end
--- spec/data_structures/definitions/date_field_spec.rb
+++ spec/data_structures/definitions/date_field_spec.rb
@@ -19,18 +19,53 @@ module DataStructures
         end
 
         describe "legal values" do
-          it "allows today"
-          it "allows yesterday"
-          it "allows tomorrow"
-          it "allows a specific date"
-          it "allows a number of days from today: 1d"
-          it "allows a number of days before today: -1d"
-          it "allows a number of weeks from today: 1w"
-          it "allows a number of weeks before today: -1w"
-          it "allows a number of months from today: 1m"
-          it "allows a number of months before today: -1m"
-          it "allows a number of years from today: 1y"
-          it "allows a number of years before today: -1y"
+          it "does not allow arbitrary strings" do
+            expect(described_class.new(caption: "First name", default: "NOT A LEGAL VALUE")).to_not be_valid
+          end
+
+          it "allows today" do
+            expect(described_class.new(caption: "First name", default: "today")).to be_valid
+          end
+
+          it "allows a number of days from today: 1d" do
+            expect(described_class.new(caption: "First name", default: "1d")).to be_valid
+            expect(described_class.new(caption: "First name", default: "200d")).to be_valid
+          end
+
+          it "allows a number of days before today: -1d" do
+            expect(described_class.new(caption: "First name", default: "-1d")).to be_valid
+            expect(described_class.new(caption: "First name", default: "-200d")).to be_valid
+          end
+
+          it "allows a number of weeks from today: 1w" do
+            expect(described_class.new(caption: "First name", default: "1w")).to be_valid
+            expect(described_class.new(caption: "First name", default: "200w")).to be_valid
+          end
+
+          it "allows a number of weeks before today: -1w" do
+            expect(described_class.new(caption: "First name", default: "-1w")).to be_valid
+            expect(described_class.new(caption: "First name", default: "-200w")).to be_valid
+          end
+
+          it "allows a number of months from today: 1m" do
+            expect(described_class.new(caption: "First name", default: "1m")).to be_valid
+            expect(described_class.new(caption: "First name", default: "200m")).to be_valid
+          end
+
+          it "allows a number of months before today: -1m" do
+            expect(described_class.new(caption: "First name", default: "-1m")).to be_valid
+            expect(described_class.new(caption: "First name", default: "-200m")).to be_valid
+          end
+
+          it "allows a number of years from today: 1y" do
+            expect(described_class.new(caption: "First name", default: "1y")).to be_valid
+            expect(described_class.new(caption: "First name", default: "200y")).to be_valid
+          end
+
+          it "allows a number of years before today: -1y" do
+            expect(described_class.new(caption: "First name", default: "-1y")).to be_valid
+            expect(described_class.new(caption: "First name", default: "-200y")).to be_valid
+          end
         end
       end
     end
