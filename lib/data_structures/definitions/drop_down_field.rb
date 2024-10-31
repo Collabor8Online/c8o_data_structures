913106fd2befa97af2ddb9f598d229de2efa780a
Rahoul Baruah <rahoulb@echodek.co>
2024-10-31 15:15:56 +0000

    Added basic drop-down field

---
+++ lib/data_structures/definitions/drop_down_field.rb
@@ -0,0 +1,16 @@
+module DataStructures
+  module Definitions
+    class DropDownField < Field
+      attr_reader :options
+      attribute :default, :string, default: ""
+      validate :default do |drop_down_field|
+        drop_down_field.errors.add :default, :is_not_legal_default unless drop_down_field.default.blank? || drop_down_field.options.include?(drop_down_field.default)
+      end
+
+      def initialize(options: {}, **)
+        super(**)
+        @options = options.transform_keys(&:to_s).freeze
+      end
+    end
+  end
+end
---
+++ spec/data_structures/definitions/drop_down_field_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+require_relative "field"
+
+module DataStructures
+  module Definitions
+    RSpec.describe DropDownField do
+      it_behaves_like "a field"
+
+      describe ".new" do
+        it "sets the options" do
+          field = described_class.new caption: "Choose an option", options: {"one" => "Option one", "two" => "Option two"}
+          expect(field.options).to eq({"one" => "Option one", "two" => "Option two"})
+        end
+
+        it "sets the options with string keys" do
+          field = described_class.new caption: "Choose an option", options: {one: "Option one", two: "Option two"}
+          expect(field.options).to eq({"one" => "Option one", "two" => "Option two"})
+        end
+
+        it "sets the default value" do
+          field = described_class.new caption: "Choose an option", default: "one", options: {one: "Option one", two: "Option two"}
+          expect(field.default).to eq "one"
+        end
+      end
+
+      describe "#default" do
+        it "defaults to an empty string" do
+          expect(described_class.new(caption: "Choose an option", options: {one: "Option one", two: "Option two"}).default).to eq ""
+        end
+
+        context "default is set" do
+          it "must be a key in the options" do
+            expect(described_class.new(caption: "Choose an option", options: {one: "Option one", two: "Option two"}, default: "two")).to be_valid
+            expect(described_class.new(caption: "Choose an option", options: {one: "Option one", two: "Option two"}, default: "three")).to_not be_valid
+          end
+        end
+
+        context "default is not set" do
+          it "is valid regardless of options" do
+            expect(described_class.new(caption: "Choose an option", options: {one: "Option one", two: "Option two"}, default: "")).to be_valid
+          end
+        end
+      end
+    end
+  end
+end
