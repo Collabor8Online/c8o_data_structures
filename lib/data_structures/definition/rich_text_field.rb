455526f0838618f0f4a200d6165ab99d6c842e69
Rahoul Baruah <rahoulb@echodek.co>
2024-11-05 13:26:55 +0000

    Store rich text values in ActionText

--- lib/data_structures/definition/rich_text_field.rb
+++ lib/data_structures/definition/rich_text_field.rb
@@ -2,6 +2,14 @@ module DataStructures
   class Definition
     class RichTextField < Field
       attribute :default, :string, default: ""
+
+      on_set_value do |item, value|
+        item.rich_text_value = value
+      end
+
+      on_get_value do |item|
+        item.rich_text_value
+      end
     end
   end
 end
